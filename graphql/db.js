89dd0a32da66d343301970f3e53e1def45702c70
변찬우 <chan00106@naver.com>
2021-11-21 18:12:36 +0900

    restAPI에서 받아오기

--- graphql/db.js
+++ graphql/db.js
@@ -1,52 +1,10 @@
-let moives = [
-  {
-    id: 0,
-    name: "이터널스",
-    score: 5.59,
-  },
-  {
-    id: 1,
-    name: "장르만 로맨스",
-    score: 6.85,
-  },
-  {
-    id: 2,
-    name: "강릉",
-    score: 6.38,
-  },
-  {
-    id: 3,
-    name: "듄",
-    score: 7.93,
-  },
-  {
-    id: 4,
-    name: "프렌치 디스패치",
-    score: 7.93,
-  },
-];
+import axios from "axios";
 
-export const getMovies = () => moives;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getId = (id) => {
-  const result = moives.filter((moive) => moive.id === id);
-  return result[0];
-};
-
-export const deleteMovie = (id) => {
-  const clear = moives.filter((movie) => movie.id !== id);
-  if (moives.length > clear.length) {
-    console.log("성공");
-    moives = clear;
-    return true;
-  } else {
-    console.log("실패");
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newData = { id: moives.length + 1, name, score };
-  moives.push(newData);
-  return newData;
+export const getMovies = async (limit) => {
+  let res;
+  if (limit <= 0) res = await axios.get(API_URL);
+  else res = await axios.get(`${API_URL}limit=${limit}`);
+  return res.data.data.movies;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,12 +2,7 @@ import { getMovies, getId, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getId(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit }) => getMovies(limit),
   },
 };
 
